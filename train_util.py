eea714c5fd5f0356de664271740c121c6302c699
Thunchakorn Suwanjiaramanee <thunchakorn@Thunchakorns-MacBook-Air.local>
2020-08-04 10:20:20 +0700

    edit train_util.py config_file

--- train_util.py
+++ train_util.py
@@ -310,7 +310,7 @@ def setup(args, train_name, test_name):
     Create configs and perform basic setups and log hyperparameter
     """
     cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file(args.config_file))
+    cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.DATASETS.TRAIN = (train_name, )
     cfg.DATASETS.TEST = (test_name, )
